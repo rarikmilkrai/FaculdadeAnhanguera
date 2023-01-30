f73ef2caa4853c8c3939ae85a37d5c9ff2d180e3
Rarikmilkrai Souza <96403081+rarikmilkrai@users.noreply.github.com>
2023-01-30 20:27:18 -0300

    Create desafio.c

---
+++ desafio.c
@@ -0,0 +1,13 @@
+// Obejtivo do programa é apresentar o nome do usuario, mae e avó.
+
+#include "stdafx.h"
+
+int main ()
+{
+    printf("Rarikmilkrai Souza \n");
+    printf("Debora Francisca minha avó \n");
+    printf("Severina Francisca minha mãe");
+    
+
+    return 0;
+ }
